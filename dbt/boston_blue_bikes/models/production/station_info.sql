4c3a258ce015a2206cb5f328a2e00cd104e16c28
Andrew Gallagher <andrew@acgallagher.com>
2023-03-14 12:37:46 -0400

    Updated dbt packages and station_info model

--- dbt/boston_blue_bikes/models/production/station_info.sql
+++ dbt/boston_blue_bikes/models/production/station_info.sql
@@ -12,10 +12,12 @@ SELECT
     status.station_status,
     status.num_docks_available,
     status.num_docks_disabled,
+    (status.num_docks_disabled / info.capacity) * 100 AS docks_disabled_pct,
     status.num_bikes_available,
+    (status.num_bikes_available / info.capacity)  * 100 AS bikes_available_pct,
     status.num_bikes_disabled,
     status.num_ebikes_available,
 FROM {{ ref('stationdata_info_most_recent') }} AS info
 JOIN {{ ref('stationdata_status_most_recent') }} AS status
     ON status.station_id = info.station_id 
-WHERE status.is_renting = true
+WHERE status.is_renting = true AND info.capacity > 0
